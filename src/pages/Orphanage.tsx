c5eaed2ac010ee1e5bbbe78e9de739f48af489c4
xavero-evandro <evandromelos@hotmail.com>
2020-10-17 22:28:06 +0200

    added new pages for orphanages

---
+++ src/pages/Orphanage.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { FiClock, FiInfo, FiArrowLeft } from "react-icons/fi";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import { useHistory } from 'react-router-dom';
+import L from 'leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanage.css';
+
+const happyMapIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60]
+})
+
+export default function Orphanage() {
+  const { goBack } = useHistory();
+
+  return (
+    <div id="page-orphanage">
+      <aside>
+        <img src={mapMarkerImg} alt="Happy" />
+
+        <footer>
+          <button type="button" onClick={goBack}>
+            <FiArrowLeft size={24} color="#FFF" />
+          </button>
+        </footer>
+      </aside>
+
+      <main>
+        <div className="orphanage-details">
+          <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+
+          <div className="images">
+            <button className="active" type="button">
+              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+            </button>
+            <button type="button">
+              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+            </button>
+            <button type="button">
+              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+            </button>
+            <button type="button">
+              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+            </button>
+            <button type="button">
+              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+            </button>
+            <button type="button">
+              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+            </button>
+          </div>
+
+          <div className="orphanage-details-content">
+            <h1>UNICEF Berlin</h1>
+            <p>Give social help to all</p>
+
+            <div className="map-container">
+              <Map
+                center={[52.5162746,13.3755154]}
+                zoom={16}
+                style={{ width: '100%', height: 280 }}
+                dragging={false}
+                touchZoom={false}
+                zoomControl={false}
+                scrollWheelZoom={false}
+                doubleClickZoom={false}
+              >
+                <TileLayer
+                  url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+                <Marker interactive={false} icon={happyMapIcon} position={[52.5162746,13.3755154]} />
+              </Map>
+
+              <footer>
+                <a href="">See on Google Maps</a>
+              </footer>
+            </div>
+
+            <hr />
+
+            <h2>Instructions for Visiting</h2>
+            <p>Come fell like home and bring much love to give.</p>
+
+            <div className="open-details">
+              <div className="hour">
+                <FiClock size={32} color="#15B6D6" />
+                Monday - Friday <br />
+                8h às 18h
+              </div>
+              <div className="open-on-weekends">
+                <FiInfo size={32} color="#39CC83" />
+                Yes <br />
+                Weekends
+              </div>
+            </div>
+
+            <button type="button" className="contact-button">
+              <FaWhatsapp size={20} color="#FFF" />
+              Get in touch
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import mapMarkerImg from '../images/map-marker.svg'
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi'
-import { Map, TileLayer } from "react-leaflet";
-
+import {FiPlus, FiArrowRight} from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup} from "react-leaflet";
+import Leaflet from 'leaflet'
 import '../styles/pages/orphanages-maps.css'
 import 'leaflet/dist/leaflet.css'
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
 
 function OrphanagesMap(){
     return (
@@ -24,12 +31,24 @@ function OrphanagesMap(){
 
             <Map
                 center={[52.5162746,13.3755154]}
-                zoom={15}
+                zoom={14}
                 style={{width: '100%', height: '100%'}}
             >
                 <TileLayer
                     url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
+
+                <Marker
+                    position={[52.5162746,13.3755154]}
+                    icon={mapIcon}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className='map-popup'>
+                        UNICEF Berlin
+                        <Link to="">
+                            <FiArrowRight size={20} color="#FFF" />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
             <Link to="" className="create-orphanage">
